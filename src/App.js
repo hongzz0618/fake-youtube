44ba6379c0a4bbd72b5deeeafc7a4ee57bb518cd
Hong <zhougg98@gmail.com>
2020-02-22 15:47:31 +0100

    print youtube video

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import youtube from "./api_youtube/youtube";
 
-async function testGet() {
-  let data = await youtube.get('search', {
-    params: {
-      part: "snippet",
-      maxResults: 10,
-      key: "AIzaSyCQrRehtFg5FhO6Hq6I-yJGKmfhi20enww",
-      q: "love",
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [], selectedVideo: null
+
     }
+  }
 
-  });
-  console.log(data);
+  componentDidMount() {
+    this.GetYoutubeInfo();
+  }
+
+  async GetYoutubeInfo() {
+    let getData = await youtube.get('search', {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        key: "AIzaSyCQrRehtFg5FhO6Hq6I-yJGKmfhi20enww",
+        q: "react",
+      }
+
+    });
+    this.setState({ videos: getData.data.items })
+
+  }
+
+  render() {
+    const { videos } = this.state
+    const listVideos = videos.map((video, index) =>
+      <div key={index}>
+        <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.channelTitle} />
+        <span>{video.snippet.title}</span>
+      </div>)
+    return (
+      <>{listVideos}</>
+    )
+  }
 }
-
-function App() {
-
-  testGet();
-  return (
-    <div className="App">
-      youtube
-    </div>
-  );
-}
-
 export default App;
