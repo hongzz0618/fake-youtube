198f17f928b14acb3f044c0e0990adbfb24c1c80
Hong <zhougg98@gmail.com>
2020-02-22 20:32:06 +0100

    improved the design of video container, and make it responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 import youtube from "./api_youtube/youtube";
+// import SearchBar from "./Component/SearchBar";
+import Videos from "./Component/Videos";
+
+import { Grid, Container } from '@material-ui/core';
 
 class App extends Component {
   constructor(props) {
@@ -19,25 +23,30 @@ class App extends Component {
     let getData = await youtube.get('search', {
       params: {
         part: "snippet",
-        maxResults: 10,
+        maxResults: 50,
         key: "AIzaSyCQrRehtFg5FhO6Hq6I-yJGKmfhi20enww",
-        q: "react",
+        q: "star",
       }
 
     });
+    console.log(getData)
     this.setState({ videos: getData.data.items })
 
   }
 
   render() {
     const { videos } = this.state
-    const listVideos = videos.map((video, index) =>
-      <div key={index}>
-        <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.channelTitle} />
-        <span>{video.snippet.title}</span>
-      </div>)
     return (
-      <>{listVideos}</>
+      <>
+        <Container maxWidth="xl">
+          {/* <SearchBar /> */}
+          <Grid container spacing={4}>
+            {videos.map((video, index) => (
+              <Videos key={index} video={video} />
+            ))}
+          </Grid>
+        </Container>
+      </>
     )
   }
 }
---
+++ src/Component/Videos.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import clsx from 'clsx';
+import { Grid, CardMedia, CardActionArea, Card, makeStyles, CardContent, CardActions, Collapse, IconButton, Typography } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles(theme => ({
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+    }
+
+}));
+export default function Videos(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const { video } = props;
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    return (
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className="videoContainer">
+            <CardActionArea>
+                <Card>
+                    <CardMedia className={classes.media} image={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {video.snippet.channelTitle}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                        <IconButton
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                            })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography variant="h6" component="h2">
+                                {video.snippet.title}
+                            </Typography>
+                            <Typography variant="subtitle2" color="textSecondary">
+                                {video.snippet.publishedAt}
+                            </Typography>
+                            <Typography variant="subtitle1" paragraph>
+                                {video.snippet.description}
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </CardActionArea>
+        </Grid>
+    );
+}
+
+Videos.propTypes = {
+    video: PropTypes.object,
+};
