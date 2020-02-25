27b7b1c2e4338fed577ecd9f7c4622e4eb5d38ff
Hong <zhougg98@gmail.com>
2020-02-25 16:58:20 +0100

    added a zoom effect in hover on images

--- src/Component/VideoList.jsx
+++ src/Component/VideoList.jsx
@@ -5,16 +5,16 @@ export default function VideoList(props) {
   const listOfVideos = props.videos.map(video => (
     <Grid item xs={12}>
       <Paper style={{ display: "flex", cursor: "pointer", maxHeight: 100 }} onClick={() => props.onVideoSelect(video)} >
-        <Grid item xs={4} md={5} >
+        <Grid item xs={4} md={5} style={{ overflow: "hidden" }} >
           <img style={{ backgroundSize: "cover", width: "100%", height: "100%" }} alt={video.snippet.channelTitle} src={video.snippet.thumbnails.medium.url} />
         </Grid>
         <Grid item xs={8} md={7}>
-          <Typography variant="subtitle2" style={{ padding: 1 }}>
+          <Typography variant="subtitle2" style={{ padding: 10 }}>
             <b>{video.snippet.title}</b>
           </Typography>
         </Grid>
-      </Paper>
-    </Grid>
+      </Paper >
+    </Grid >
   ));
 
   return (
